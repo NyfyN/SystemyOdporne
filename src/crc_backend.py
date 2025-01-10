c99be06acbb1c7b226242af4625d364d1965a454
NyfyN <67443078+NyfyN@users.noreply.github.com>
2025-01-10 10:30:22 +0100

    Update crc_backend.py

--- src/crc_backend.py
+++ src/crc_backend.py
@@ -16,22 +16,22 @@ def binary_to_text(binary):
     return ''.join(chr(int(char, 2)) for char in chars)
 
 
-# def crc_encode(data, polynomial):
-#     """
-#     Oblicza CRC dla dowolnego tekstu przy użyciu zadanego wielomianu.
-#     """
-#     data_binary = text_to_binary(data)  # Zamiana tekstu na binarny ciąg
-#     data_bits = list(data_binary)
-#     poly_bits = list(polynomial)
-#     data_bits.extend(['0'] * (len(poly_bits) - 1))  # Dodanie bitów zerowych
+def crc_encode(data, polynomial):
+    """
+    Oblicza CRC dla dowolnego tekstu przy użyciu zadanego wielomianu.
+    """
+    data_binary = text_to_binary(data)  # Zamiana tekstu na binarny ciąg
+    data_bits = list(data_binary)
+    poly_bits = list(polynomial)
+    data_bits.extend(['0'] * (len(poly_bits) - 1))  # Dodanie bitów zerowych
 
-#     for i in range(len(data_bits) - len(poly_bits) + 1):
-#         if data_bits[i] == '1':
-#             for j in range(len(poly_bits)):
-#                 data_bits[i + j] = str(int(data_bits[i + j] != poly_bits[j]))
+    for i in range(len(data_bits) - len(poly_bits) + 1):
+        if data_bits[i] == '1':
+            for j in range(len(poly_bits)):
+                data_bits[i + j] = str(int(data_bits[i + j] != poly_bits[j]))
 
-#     remainder = ''.join(data_bits[-(len(poly_bits) - 1):])  # Reszta CRC
-#     return data_binary + remainder  # Dołączenie reszty CRC do oryginalnego ciągu
+    remainder = ''.join(data_bits[-(len(poly_bits) - 1):])  # Reszta CRC
+    return data_binary + remainder  # Dołączenie reszty CRC do oryginalnego ciągu
 
 
 def highlight_errors(message, errors):
@@ -71,101 +71,52 @@ def introduce_error(message, error_rate):
             message[i] = '1' if message[i] == '0' else '0'
     return ''.join(message)
 
-def xor(a,b):
-    	result = []
-		for i in range(1,len(b)):
-			if a[i] == b[i]:
-				result.append('0')
-			else:
-				result.append('1')
-		return  ''.join(result)
+def crc_validate(received_data, polynomial):
+    """
+    Sprawdza wiadomość z kodem CRC i zwraca indeksy błędnych bitów.
+    """
+    data = list(received_data)
+    poly = list(polynomial)
+    # Oblicz CRC na podstawie danych wejściowych
+    encoded_data = data[:]
+    encoded_data.extend(['0'] * (len(poly) - 1))
+    for i in range(len(encoded_data) - len(poly) + 1):
+        if encoded_data[i] == '1':
+            for j in range(len(poly)):
+                encoded_data[i + j] = str(int(encoded_data[i + j] != poly[j]))
 
-def crc_validate(message, key):
-    pick = len(key)
+    # Zwracaj miejsca, w których występuje błąd
+    remainder = ''.join(encoded_data[-(len(poly) - 1):])
+    errors = [i for i, bit in enumerate(remainder) if bit != '0']
+    print(f"Validation errors at positions: {errors}")  # Debug print
+    return errors
 
-    tmp = message[:pick]
 
-    while pick < len(message):
-        if tmp[0] == '1':
-            tmp_holder = xor(key,tmp)+message[pick]
+def crc_correct(received_data, polynomial):
+    """
+    Poprawia błędy bitowe w binarnym kodzie po obliczeniu CRC.
+    """
+    data = list(received_data)
+    poly = list(polynomial)
+    # Oblicz CRC na podstawie danych wejściowych
+    encoded_data = data[:]
+    encoded_data.extend(['0'] * (len(poly) - 1))
+    for i in range(len(encoded_data) - len(poly) + 1):
+        if encoded_data[i] == '1':
+            for j in range(len(poly)):
+                encoded_data[i + j] = str(int(encoded_data[i + j] != poly[j]))
+
+    # Znajdź miejsca, w których występuje błąd
+    remainder = ''.join(encoded_data[-(len(poly) - 1):])
+    errors = [i for i, bit in enumerate(remainder) if bit != '0']
+    print(f"Correction errors at positions: {errors}")  # Debug print
+
+    # Popraw błędy tylko na pozycjach ze zmiennej errors
+    corrected_data = list(received_data)
+    for error in crc_validate(received_data, polynomial):
+        if corrected_data[error] == "1":
+            corrected_data[error] = "0"
         else:
-            tmp_holder = xor('0'*pick,tmp) + message[pick]
+            corrected_data[error] = "1"
 
-        pick+=1
-
-    if tmp[0] == "1":
-        tmp = xor(key,tmp)
-    else:
-        tmp = xor('0'*pick,tmp)
-
-    checkword = tmp
-    return checkword
-
-def encodedData(self,data,key):
-    l_key = len(key)
-    append_data = data + '0'*(l_key-1)
-    remainder = self.crc(append_data,key)
-    codeword = data+remainder
-    self.cdw += codeword
-    print("Remainder: " ,remainder)
-    print("Data: " ,codeword)
-
-def reciverSide(self,key,data):
-    r = self.crc(data,key)
-    size = len(key)
-    print(r)
-    if r == size*0:
-        print("No Error")
-    else:
-        print("Error")
-
-
-# def crc_validate(received_data, polynomial):
-#     """
-#     Sprawdza wiadomość z kodem CRC i zwraca indeksy błędnych bitów.
-#     """
-#     data = list(received_data)
-#     poly = list(polynomial)
-#     # Oblicz CRC na podstawie danych wejściowych
-#     encoded_data = data[:]
-#     encoded_data.extend(['0'] * (len(poly) - 1))
-#     for i in range(len(encoded_data) - len(poly) + 1):
-#         if encoded_data[i] == '1':
-#             for j in range(len(poly)):
-#                 encoded_data[i + j] = str(int(encoded_data[i + j] != poly[j]))
-
-#     # Zwracaj miejsca, w których występuje błąd
-#     remainder = ''.join(encoded_data[-(len(poly) - 1):])
-#     errors = [i for i, bit in enumerate(remainder) if bit != '0']
-#     print(f"Validation errors at positions: {errors}")  # Debug print
-#     return errors
-
-
-# def crc_correct(received_data, polynomial):
-#     """
-#     Poprawia błędy bitowe w binarnym kodzie po obliczeniu CRC.
-#     """
-#     data = list(received_data)
-#     poly = list(polynomial)
-#     # Oblicz CRC na podstawie danych wejściowych
-#     encoded_data = data[:]
-#     encoded_data.extend(['0'] * (len(poly) - 1))
-#     for i in range(len(encoded_data) - len(poly) + 1):
-#         if encoded_data[i] == '1':
-#             for j in range(len(poly)):
-#                 encoded_data[i + j] = str(int(encoded_data[i + j] != poly[j]))
-
-#     # Znajdź miejsca, w których występuje błąd
-#     remainder = ''.join(encoded_data[-(len(poly) - 1):])
-#     errors = [i for i, bit in enumerate(remainder) if bit != '0']
-#     print(f"Correction errors at positions: {errors}")  # Debug print
-
-#     # Popraw błędy tylko na pozycjach ze zmiennej errors
-#     corrected_data = list(received_data)
-#     for error in crc_validate(received_data, polynomial):
-#         if corrected_data[error] == "1":
-#             corrected_data[error] = "0"
-#         else:
-#             corrected_data[error] = "1"
-
-#     return ''.join(corrected_data)
+    return ''.join(corrected_data)
